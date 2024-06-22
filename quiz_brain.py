9ea78ca2801e7282b78e2d3a687f21eb15a59df5
gpib <rafal.koslowski@gmail.com>
2024-06-22 11:04:31 +0200

    second commit, extra files

---
+++ quiz_brain.py
@@ -0,0 +1,23 @@
+
+
+class QuizBrain:
+    def __init__(self, question_list_input):
+        self.question_number = 0
+        self.question_list = question_list_input
+        self.still_question = True
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {self.current_question.text} (True/ False):")
+
+    def still_has_questions (self):
+        return self.question_number < len(self.question_list)
+
+
+
+
+
+
+
+
