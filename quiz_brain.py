326c35ab4147c655cc7632a32f3792a25d17b711
gpib <rafal.koslowski@gmail.com>
2024-06-22 12:01:16 +0200

    quiz_brain.py file includes score counting

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,15 +4,26 @@ class QuizBrain:
     def __init__(self, question_list_input):
         self.question_number = 0
         self.question_list = question_list_input
-        self.still_question = True
+        self.score = 0
 
     def next_question(self):
-        self.current_question = self.question_list[self.question_number]
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {self.current_question.text} (True/ False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/ False):")
+        self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions (self):
         return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You are right")
+            self.score += 1
+        else:
+            print("You are wrong")
+        print(f"The correct answer was {correct_answer}")
+        print(f"Your score is {self.score}/{self.question_number}")
+
 
 
 
